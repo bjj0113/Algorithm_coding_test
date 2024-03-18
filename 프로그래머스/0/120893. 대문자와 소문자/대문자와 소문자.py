02c850f8dcf5ca4e279ce82b0a82321e136ccac6
BaeJaeJun <159032083+bjj0113@users.noreply.github.com>
2024-03-18 16:57:06 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.15 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,9 @@
+def solution(my_string):
+    answer = ''
+    for x in my_string:
+        if(x.isupper()):
+            answer+=x.lower()
+        else:
+            answer+=x.upper()
+            
+    return answer
