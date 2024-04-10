9789d4c6942a5737b245bc4fd24d1cbad2782d27
BaeJaeJun <159032083+bjj0113@users.noreply.github.com>
2024-04-10 23:59:17 +0900

    [level 0] Title: 0 떼기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,8 +1,7 @@
 def solution(n_str):
-    strlist = list(n_str)
     
-    for i in range(len(strlist)):
-        if strlist[i] == "0":
+    for i in range(len(n_str)):
+        if n_str[i] == "0":
             continue
         else:
             return n_str[i:]
