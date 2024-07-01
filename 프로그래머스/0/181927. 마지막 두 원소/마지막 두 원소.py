87e6364ba2fe5600bd9cd77d86df52389742bdba
BaeJaeJun <159032083+bjj0113@users.noreply.github.com>
2024-07-01 14:30:42 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    if num_list[-1] > num_list[-2]:
+        answer = num_list[-1] - num_list[-2]
+    else:
+        answer = num_list[-1] * 2
+        
+    num_list.append(answer)
+    
+    return num_list
