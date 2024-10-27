28975a408111c8e3b3b2ddd6fb206990a0c552de
BaeJaeJun <159032083+bjj0113@users.noreply.github.com>
2024-10-27 18:01:22 +0900

    [level 0] Title: 배열 만들기 2, Time: 800.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.py
@@ -2,11 +2,10 @@ def solution(l, r):
     answer = []
     for i in range(l,r+1):
         x = i
-        y = str(x).replace('5','')
-        y = y.replace('0','')
-        if  y == '':
+        y = set(list(str(x))) - set('05')
+        
+        if not y:
             answer.append(i)
-    
     if not answer:
         answer.append(-1)
     return answer
