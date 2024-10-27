0b0ec515e7234ab325e706f667263a82a379949e
BaeJaeJun <159032083+bjj0113@users.noreply.github.com>
2024-10-27 14:19:54 +0900

    [level 0] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -0,0 +1,7 @@
+def solution(x1, x2, x3, x4):
+    
+    if (x1 == False and x2 == False or x3 == False and x4 == False):
+        answer = False
+    else:
+        answer = True
+    return answer
