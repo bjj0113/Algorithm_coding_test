29dd86909e0b5e0b26e333b347b5467aa5701bd2
BaeJaeJun <159032083+bjj0113@users.noreply.github.com>
2024-11-25 22:57:37 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 336 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,12 @@
+def sol(a,b,c,d,e,f):
+    
+    for x in range(-999,1000):
+        for y in range(-999,1000):
+            if a*x+b*y == c and d*x+e*y == f:
+                print(f'{int(x)} {int(y)}')
+
+
+
+a,b,c,d,e,f = map(int,input().split())
+sol(a,b,c,d,e,f)
+
